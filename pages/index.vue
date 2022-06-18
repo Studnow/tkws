c0930a6da0738ff9811632ca46bde7c561d942fe
Igor <silent659@gmail.com>
2022-06-18 03:53:18 +0300

    second collection doesn't work

--- pages/index.vue
+++ pages/index.vue
@@ -2,38 +2,48 @@
   <!-- <Tutorial :posts="posts"/> -->
   <div id="app">
     <Header />
-    <PostList :posts="posts" />
-    <SomeList :some="some" />
-    <NuxtContent :document="doc" />
+    <div class="mx-auto flex mt-8">
+      <div class="blog mx-auto">
+        <h3 class="font-bold text-xl">Links from blog collection</h3>
+        <PostList :posts="posts" />
+      </div>
+      <div class="some mx-auto">
+        <h4 class="font-bold text-xl">Links from some collection</h4>
+        <SomeList :some="some" />
+      </div>
+    </div>
+    <NuxtContent class="mt-8" :document="doc" />
     <!-- <NuxtContent :document="some" /> -->
   </div>
 </template>
 
 <script>
-import Header from '~/components/Header.vue';
+import Header from '~/components/Header.vue'
 export default {
-    name: "IndexPage",
-    head() {
-        return {
-            script: [{ src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }],
-        };
-    },
-    async asyncData({ $content }) {
-        const posts = await $content("blog").fetch();
-        const doc = await $content("hello").fetch();
-        const some = await $content("some").fetch();
-        return {
-            posts,
-            doc,
-            some
-        };
-    },
-    // async asyncData({ $content }) {
-    //     const doc = await $content("hello").fetch();
-    //     return {
-    //         doc,
-    //     };
-    // },
-    components: { Header }
+  name: 'IndexPage',
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    }
+  },
+  async asyncData({ $content }) {
+    const posts = await $content('blog').fetch()
+    const doc = await $content('hello').fetch()
+    const some = await $content('some').fetch()
+    return {
+      posts,
+      doc,
+      some,
+    }
+  },
+  // async asyncData({ $content }) {
+  //     const doc = await $content("hello").fetch();
+  //     return {
+  //         doc,
+  //     };
+  // },
+  components: { Header },
 }
 </script>
